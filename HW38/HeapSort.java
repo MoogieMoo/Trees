5a9e65ff662e663930e364bf12944705842f15bc
MoogieMoo <yxia1@stuy.edu>
2017-05-10 22:55:13 -0400

    Update HeapSort.java

--- HW38/HeapSort.java
+++ HW38/HeapSort.java
@@ -1,6 +1,6 @@
 // Maggie Xia
 // APCS2 pd1
-// HW39 -- Heaping Piles of Sordidness
+// HW38 -- Sort of Like Magic
 // 2017-05-11
 
 /*****************************************************
@@ -8,4 +8,57 @@
  * Uses  ALHeap to sort a dataset
  *****************************************************/
 
-public class HeapSort {
+import java.util.ArrayList;
+
+public class HeapSort
+{
+    //instance vars
+    private ArrayList<Integer> _data;
+    private ALHeap _heap;
+
+    /*****************************************************
+     * default constructor  ---  inits empty heap and ALHeap
+     *****************************************************/
+    public HeapSort()
+    {
+	_heap = new ALHeap();
+    }
+
+    /*****************************************************
+     * add(Integer) 
+     * Inserts an element in the ALHeap to sort
+     * Postcondition: element is placed in its final resting pos
+     *****************************************************/
+    public void add( Integer addVal ) 
+    { 
+	_heap.add( addVal );//add the value to the ALHeap
+    }
+
+    /*****************************************************
+     * sort() 
+     * Returs a sorted data set of the added values 
+     *****************************************************/
+    public ArrayList<Integer> sort() 
+    { 
+	while ( _heap.removeMin() != null ) {
+	    _data.add( _heap.removeMin() );
+	}
+	return _data;
+    }
+
+    //main method for testing
+    public static void main( String[] args ) 
+    {
+	HeapSort pile = new HeapSort();
+
+	//add in random values to sort
+	for ( int i = 0; i < 6; i ++ ) {
+	    int rand = (int) ( Math.random() * 10 ) + 1;
+	    pile.add( rand );
+	    System.out.println( "Adding " + rand + "to data set... " );
+	}
+
+	System.out.println( pile.sort() );//returns sorted data set
+    }
+    
+}//end class HeapSort

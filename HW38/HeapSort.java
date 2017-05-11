b21fe8669bae958ab69250ee59381fb35cee1cb3
MoogieMoo <yxia1@stuy.edu>
2017-05-10 22:47:32 -0400

    Add files via upload

---
+++ HW38/HeapSort.java
@@ -0,0 +1,11 @@
+// Maggie Xia
+// APCS2 pd1
+// HW39 -- Heaping Piles of Sordidness
+// 2017-05-11
+
+/*****************************************************
+ * class HeapSort
+ * Uses  ALHeap to sort a dataset
+ *****************************************************/
+
+public class HeapSort {
